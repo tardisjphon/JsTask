f399a1d7eaaf6d4c8a1aaba0479a18d4f5caac34
jerzySz <jerzy.szczepanski@ipos.pl>
2023-05-21 10:39:15 +0200

    clean up

--- domain/src/main/java/js/task/domain/usecase/GetDataUseCase.kt
+++ domain/src/main/java/js/task/domain/usecase/GetDataUseCase.kt
@@ -3,7 +3,6 @@ package js.task.domain.usecase
 import js.task.data.db.model.DataModel
 import js.task.domain.model.GetDataResponse
 
-
 interface GetDataUseCase
 {
     suspend fun invokeGetData(dataList: ArrayList<DataModel>) : GetDataResponse
