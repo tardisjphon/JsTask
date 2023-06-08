ab17639b66f5b414cb31db07d60f1fa9538431a0
jerzySz <jerzy.szczepanski@ipos.pl>
2023-06-08 19:56:59 +0200

    Kotlin to Java -> GithubPage

---
+++ data/src/main/java/js/task/data/local/db/model/GithubPage.java
@@ -0,0 +1,113 @@
+package js.task.data.local.db.model;
+
+public class GithubPage
+{
+    private String login;
+    private int id;
+    private String node_id;
+    private String avatar_url;
+    private String gravatar_id;
+    private String url;
+    private String html_url;
+    private String followers_url;
+    private String following_url;
+    private String gists_url;
+    private String starred_url;
+    private String subscriptions_url;
+    private String organizations_url;
+    private String repos_url;
+    private String events_url;
+    private String received_events_url;
+    private String type;
+    private Boolean site_admin;
+
+    public String getLogin()
+    {
+        return login;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getNode_id()
+    {
+        return node_id;
+    }
+
+    public String getAvatar_url()
+    {
+        return avatar_url;
+    }
+
+    public String getGravatar_id()
+    {
+        return gravatar_id;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public String getHtml_url()
+    {
+        return html_url;
+    }
+
+    public String getFollowers_url()
+    {
+        return followers_url;
+    }
+
+    public String getFollowing_url()
+    {
+        return following_url;
+    }
+
+    public String getGists_url()
+    {
+        return gists_url;
+    }
+
+    public String getStarred_url()
+    {
+        return starred_url;
+    }
+
+    public String getSubscriptions_url()
+    {
+        return subscriptions_url;
+    }
+
+    public String getOrganizations_url()
+    {
+        return organizations_url;
+    }
+
+    public String getRepos_url()
+    {
+        return repos_url;
+    }
+
+    public String getEvents_url()
+    {
+        return events_url;
+    }
+
+    public String getReceived_events_url()
+    {
+        return received_events_url;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public Boolean getSite_admin()
+    {
+        return site_admin;
+    }
+}
