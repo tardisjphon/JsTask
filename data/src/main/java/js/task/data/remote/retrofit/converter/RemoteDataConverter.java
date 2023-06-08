bb72eed8313fa27177fae74588433e5de0306e8c
jerzySz <jerzy.szczepanski@ipos.pl>
2023-06-08 11:15:25 +0200

    Kotlin to Java -> RemoteDataConverter

--- data/src/main/java/js/task/data/remote/retrofit/converter/RemoteDataConverter.java
+++ data/src/main/java/js/task/data/remote/retrofit/converter/RemoteDataConverter.java
@@ -1,49 +1,54 @@
 package js.task.data.remote.retrofit.converter;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
 import javax.inject.Inject;
+
 import js.task.data.local.db.model.DailyMotionPage;
 import js.task.data.local.db.model.DataModel;
 import js.task.data.local.db.model.GithubPage;
 import js.task.data.remote.retrofit.converter.model.ApiName;
 
 
-//TODO
-
-public class RemoteDataConverter
-{
+public class RemoteDataConverter {
     @Inject
-    public RemoteDataConverter() {}
-
-    public List<DataModel> getDataModel(DailyMotionPage data)
-    {
-        int index = 0;
-        return Objects.requireNonNull(data.getList()).stream().map (
-                dailyMotion ->
-
-            new DataModel(0,
-                    index, //TODO przepisywalny
-                    dailyMotion.getScreenname(),
-                    "",
-                    ApiName.DAILY_MOTION
-            )).collect(Collectors.toList());
-
-        //.sortedByDescending { it.apiName?.name } ?: ArrayList()
+    public RemoteDataConverter() {
     }
 
-    public List<DataModel> getDataModel(List<GithubPage> data)
-    {
-        return data.stream().map( mapping ->
-            new DataModel(
-                    0, //TODO nie wstawiac
-                    mapping.getId(),
-                    mapping.getLogin(),
-                    mapping.getAvatar_url(),
-                    ApiName.GITHUB
-        )).collect(Collectors.toList());
+    public List<DataModel> getDataModel(DailyMotionPage data) {
+        if (data.getList() == null) {
+            return new ArrayList<>();
+        }
 
-        //.sortedByDescending { it.apiName?.name }
+        AtomicInteger index = new AtomicInteger();
+        return data.getList().stream().map(
+                dailyMotion ->
+                        new DataModel(
+                                null,
+                                index.getAndIncrement(),
+                                dailyMotion.getScreenname(),
+                                "",
+                                ApiName.DAILY_MOTION
+                        )).sorted(getApiNameComparator()).collect(Collectors.toList());
+    }
+
+    public List<DataModel> getDataModel(List<GithubPage> data) {
+        return data.stream().map(mapping ->
+                new DataModel(
+                        null,
+                        mapping.getId(),
+                        mapping.getLogin(),
+                        mapping.getAvatar_url(),
+                        ApiName.GITHUB
+                )).sorted(getApiNameComparator()).collect(Collectors.toList());
+    }
+
+    private Comparator<DataModel> getApiNameComparator() {
+        return Comparator.comparing(m -> Objects.requireNonNull(m.getApiName()).name());
     }
 }
