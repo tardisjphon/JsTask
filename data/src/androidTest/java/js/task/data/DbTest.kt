960e0e5d19ae0c856c48d720051685ec99f848c7
jerzySz <jerzy.szczepanski@ipos.pl>
2023-06-09 12:05:52 +0200

    code cleaning

--- data/src/androidTest/java/js/task/data/DbTest.kt
+++ data/src/androidTest/java/js/task/data/DbTest.kt
@@ -23,7 +23,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class DbTest
-{ //    @Test
+{
+    //    @Test
     //    fun useAppContext() {
     //        // Context of the app under test.
     //        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
