96ba5d370a363f9188006200e5f90feda1d34581
jerzySz <jerzy.szczepanski@ipos.pl>
2023-06-27 08:53:24 +0200

    MainActivityTest added

---
+++ presentation/src/test/java/js/task/views/MainActivityTest.kt
@@ -0,0 +1,33 @@
+package js.task.views
+
+import js.task.screens.MainActivity
+import junit.framework.TestCase.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+
+@RunWith(RobolectricTestRunner::class)
+class MainActivityTest
+{
+    private var activity : MainActivity? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp()
+    {
+        activity = Robolectric.buildActivity(MainActivity::class.java)
+            .create()
+            .resume()
+            .get()
+    }
+
+    @Test
+    @Throws(java.lang.Exception::class)
+    fun shouldNotBeNull()
+    {
+        assertNotNull(activity)
+    }
+}
