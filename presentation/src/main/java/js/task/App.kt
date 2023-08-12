a0b84a9d75a820141589fef639d1f7bfdce6414b
jerzySz <jerzy.szczepanski@ipos.pl>
2023-08-12 14:02:48 +0200

    koin modules added

--- presentation/src/main/java/js/task/App.kt
+++ presentation/src/main/java/js/task/App.kt
@@ -1,22 +1,63 @@
 package js.task
 
 import android.app.Application
-import js.task.data.di.DataProviderModule
-import js.task.di.ApplicationGraph
-import js.task.di.DaggerApplicationGraph
-import js.task.di.DataViewModelModule
+import js.task.data.DataProvider
+import js.task.data.local.DbRepository
+import js.task.data.local.db.AppDatabase
+import js.task.data.remote.RetrofitRepository
+import js.task.domain.interfaces.IDataProvider
+import js.task.domain.usecase.GetDataUseCase
+import js.task.domain.usecase.interfaces.IGetDataUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
+
 
 class App : Application()
 {
-    lateinit var applicationGraph : ApplicationGraph
+    private val coroutineScope : CoroutineScope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate()
     {
-        applicationGraph = DaggerApplicationGraph.builder()
-        .dataProviderModule(DataProviderModule(this))
-        .dataViewModelModule(DataViewModelModule())
-        .build()
+        val moduleDataProvider = module {
+            single {
+                getDataProvider()
+            }
+        }
+        val moduleDataUseCase = module {
+            single {
+                getDataUseCase()
+            }
+        }
+
+        startKoin {
+            modules(
+                    moduleDataProvider,
+                    moduleDataUseCase
+            )
+        }
 
         super.onCreate()
+    }
+
+    private fun getDataProvider() : IDataProvider
+    {
+        return DataProvider(
+                coroutineScope,
+                DbRepository(
+                        AppDatabase.getInstance(applicationContext),
+                        coroutineScope
+                ),
+                RetrofitRepository()
+        )
+    }
+
+    private fun getDataUseCase() : IGetDataUseCase
+    {
+        return GetDataUseCase(
+                coroutineScope,
+                getDataProvider()
+        )
     }
 }
